95567f52e8aa9269eb45998aa51a0e27429a9cd6
ManthenaKarthik <manthenakarthik604@gmail.com>
2026-02-08 22:34:10 +0530

    Add files via upload

---
+++ if else.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main()
+{ 
+int a ;
+printf ("Enter the value of a :");
+scanf("%d",&a);
+if (a%2==0)
+{printf("%d is even number \n",a);
+}
+else
+{ 
+    printf("%d is odd number \n",a);
+}
+}
